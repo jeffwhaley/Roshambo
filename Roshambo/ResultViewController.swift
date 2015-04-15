4a3a7889731f598bb6fe322e00296e2dd6d3563b
jeffwhaley <jeffwhaley@gmail.com>
2015-04-15 11:39:22 -0500

    Get working for rock

---
+++ Roshambo/ResultViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ResultViewController.swift
+//  Roshambo
+//
+//  Created by Jeff Whaley on 4/15/15.
+//  Copyright (c) 2015 Jeff Whaley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    var userThrow: Throw?
+    var computerThrow: Throw?
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    override func viewWillAppear(animated: Bool) {
+        if userThrow == computerThrow {
+            resultImage.image = UIImage(named: "itsATie.png")
+            resultLabel.text = "Tied."
+        
+        } else if userThrow == .paper {
+            if computerThrow == .rock {
+                resultImage.image = UIImage(named: "PaperCoversRock.jpg")
+                resultLabel.text = "You Win!"
+            } else if computerThrow == .scissors {
+                resultImage.image = UIImage(named: "ScissorsCutPaper.jpg")
+                resultLabel.text = "You Lose!"
+            }
+
+        } else if userThrow == .rock {
+            if computerThrow == .paper {
+                resultImage.image = UIImage(named: "PaperCoversRock.jpg")
+                resultLabel.text = "You Lose!"
+            } else if computerThrow == .scissors {
+                resultImage.image = UIImage(named: "RockCrushesScissors.jpeg")
+                resultLabel.text = "You Win!"
+            }
+        } else if userThrow == .scissors {
+            if computerThrow == .rock {
+                resultImage.image = UIImage(named: "RockCrushesScissors.jpeg")
+                resultLabel.text = "You Lose!"
+            } else if computerThrow == .paper {
+                resultImage.image = UIImage(named: "ScissorsCutPaper.jpg")
+                resultLabel.text = "You Win!"
+            }
+            
+        }
+        resultImage.alpha = 0
+        resultLabel.alpha = 0
+        playAgainButton.alpha = 0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(0.3) {
+            self.resultImage.alpha = 1
+            self.resultLabel.alpha = 1
+            self.playAgainButton.alpha = 1
+        }
+        
+    }
+    
+    @IBAction func playAgain(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
--- Roshambo/ThrowViewController.swift
+++ Roshambo/ThrowViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
+enum Throw {
+    case rock, paper, scissors
+}
+
 class ThrowViewController: UIViewController {
+    
+    func randomThrow() -> Throw {
+        // generate a random throw from
+        var t:Throw
+        
+        let r = 1 + arc4random() % 3
+        
+        if r == 1 {
+            t = .rock
+        } else if r == 2 {
+            t = .paper
+        } else {
+            t = .scissors
+        }
+        
+        return t
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func rockButton(sender: AnyObject) {
+        var controller: ResultViewController
+        controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultViewController") as! ResultViewController
+        
+        controller.userThrow = .rock
+        controller.computerThrow = randomThrow()
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+    }
+    @IBAction func paperButton(sender: AnyObject) {
+    }
+    @IBAction func scissorsButton(sender: AnyObject) {
     }
     
 }
